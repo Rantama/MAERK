234c881bc4ddc5c109b03ebf6b1e67865da32f1b
Long Pham <longpham@Ksquares-MacBook-Pro-2.local>
2016-10-25 13:28:07 -0500

    worked on navbar added logo and log in

--- client/app/app.js
+++ client/app/app.js
@@ -3,8 +3,10 @@
 angular.module('maerkApp', ['maerkApp.auth', 'maerkApp.admin', 'maerkApp.constants', 'ngCookies',
     'ngResource', 'ngSanitize', 'ui.router', 'validation.match', 'ngMaterial'
   ])
-  .config(function($urlRouterProvider, $locationProvider) {
+  .config(function($urlRouterProvider, $locationProvider, $mdThemingProvider) {
     $urlRouterProvider.otherwise('/');
-
+    $mdThemingProvider.theme('default')
+        .primaryPalette('pink')
+        .accentPalette('orange');
     $locationProvider.html5Mode(true);
   });
--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -36,3 +36,10 @@
       controller: MainController
     });
 })();
+
+var app = angular.module('myApp', ['ngMaterial']);
+app.controller('MyController', function($scope, $mdSidenav) {
+  $scope.openLeftMenu = function() {
+    $mdSidenav('left').toggle();
+  };
+});
