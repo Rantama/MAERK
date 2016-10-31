51146f08eaced4793a72e0e92bf311b62877ae23
Long Pham <longpham@Ksquares-MacBook-Pro-2.local>
2016-10-31 15:11:12 -0500

    fixed route authentication and missing route for getting one employee

--- server/api/employee/index.js
+++ server/api/employee/index.js
@@ -7,10 +7,10 @@ import * as controller from './employee.controller';
 import * as auth from '../../auth/auth.service';
 
 
-var router = Router();
+var router = new Router();
 
-router.get('/', controller.index);
-router.post('/', controller.create);
-router.put('/:id', controller.update);
+router.get('/', auth.hasRole('admin')controller.index);
+router.post('/', auth.hasRole('admin')controller.create);
+router.put('/:id', auth.hasRole('admin')controller.update);
 
 module.exports = router;
